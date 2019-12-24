73e42f6d0edbd1695ca18547b69363df3c97f86a
Anna Dybas <a.m.dybas@gmail.com>
2019-12-24 17:50:39 -0500

    Done.

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -2,20 +2,21 @@ module FancyDance
     module InstanceMethods
         def twirl
             "I'm twirling!"
-          end
+        end
        
-          def jump
+        def jump
             "Look how high I'm jumping!"
-          end
+        end
        
-          def pirouette
+        def pirouette
             "I'm doing a pirouette"
-          end
+        end
        
-          def take_a_bow
+        def take_a_bow
             "Thank you, thank you. It was a pleasure to dance for you all."
-          end
+        end
     end
+    
     module ClassMethods
         def metadata
             "This class produces objects that love to dance."
